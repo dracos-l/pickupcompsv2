0e7cc9fd4f229b2af283b4a4a3f05a4378dd55e8
dracos-l <logandracos@icloud.com>
2023-09-19 16:57:26 -0400

    Ok I think we're good

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -23,18 +23,18 @@ const DropdownMenu = () => {
   return (
     <div>
         <div className="dropdown">
-        <button className="dropdown-button" onClick={toggleMenu}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-            </svg>
-        </button>
-        {isOpen && (
-            <ul className="dropdown-menu">
-            <li onClick={() => handleItemClick('Contact')}>Contact Us</li>
-            <li onClick={() => handleItemClick('AboutUs')}>About Us</li>
-            <li onClick={() => handleItemClick('AboutProj')}>About the Project</li>
-            </ul>
-        )}
+          <button className="dropdown-button" onClick={toggleMenu}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+          </button>
+          {isOpen && (
+              <ul className="dropdown-menu">
+                <li onClick={() => handleItemClick('Contact')}>Contact Us</li>
+                <li onClick={() => handleItemClick('AboutUs')}>About Us</li>
+                <li onClick={() => handleItemClick('AboutProj')}>About the Project</li>
+              </ul>
+          )}
         </div>
         {isPopupOpen && (
             <div className="popup">
