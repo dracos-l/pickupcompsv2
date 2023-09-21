920bb813d67d8450910a158c1d932d88f9d95356
dracos-l <logandracos@icloud.com>
2023-09-21 10:50:32 -0400

    Work with popup and dropdown

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,51 +1,48 @@
 import React, { useState } from 'react';
 
-
 const DropdownMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
-  const handleItemClick = (item) => {
-    setIsOpen(false);
-    setSelectedItem(item);
-    setIsPopupOpen(true); // Open the popup when an item is selected
-  };
+    const handleItemClick = (item) => {
+        setIsOpen(false);
+        setSelectedItem(item);
+        setIsPopupOpen(true); // Open the popup when an item is selected
+    };
 
-  const closePopup = () => {
-    setIsPopupOpen(false);
-  };
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    };
 
 
-  return (
-    <div>
-        <div class="dropdown">
-          <button class="dropdown-button" onClick={toggleMenu}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-              </svg>
-          </button>
-          {isOpen && (
-              <ul class="dropdown-menu">
-                <li onClick={() => handleItemClick('Contact')}>Contact Us</li>
-                <li onClick={() => handleItemClick('AboutUs')}>About Us</li>
-                <li onClick={() => handleItemClick('AboutProj')}>About the Project</li>
-              </ul>
-          )}
-        </div>
-        {isPopupOpen && (
-            <div class="popup">
-                <div class="popup-content">
-                    <h3>Custom Popup Text for {selectedItem}</h3>
-                    <button onClick={closePopup}>Close</button>
-                </div>
+    return (
+        <div>
+            <div class="dropdown">
+            <button class="dropdown-button" onClick={toggleMenu}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+            </button>
+            {isOpen && (
+                <ul class="dropdown-menu">
+                    <li onClick={() => handleItemClick('Contact')}>Contact Us</li>
+                    <li onClick={() => handleItemClick('AboutUs')}>About Us</li>
+                    <li onClick={() => handleItemClick('AboutProj')}>About the Project</li>
+                </ul>
+            )}
             </div>
-        )}
-    </div>
-  )}
+            {isPopupOpen && (
+                <div class="popup">
+                        <h3>Custom Popup Text for {selectedItem}</h3>
+                        <button onClick={closePopup}>Close</button>
+                </div>
+            )}
+        </div>
+    );}
 
 export default DropdownMenu;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,15 @@
 import React from 'react';
 import DropdownMenu from './DropdownMenu';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/NavBar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Container from 'react-bootstrap/Container';
+
 
 const Header = () => {
   return (
     <header>
+      <h1>(HOME BUTTON/LOGO)</h1>
       <nav>
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" fixed="top">
-          <Container fluid>
-            <Navbar.Brand href="#home">
-                React-Bootstrap
-            </Navbar.Brand>
-
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav class="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">
-                    About Us
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    About the Project
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Contact Us
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <DropdownMenu />
       </nav>
-  </header>
+    </header>
   );
 }
 
