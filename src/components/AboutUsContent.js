88359263876d9e31dac819e0050982854b68b44e
dracos-l <logandracos@icloud.com>
2023-10-01 18:59:31 -0400

    Popup content work

--- src/components/AboutUsContent.js
+++ src/components/AboutUsContent.js
@@ -4,13 +4,28 @@ const AboutUsContent = () => {
   return (
     <div class="scrollable-content">
 
-        <p>Our team is composed of 4 members. We are all students at the University of California, San Diego. We are all in the Computer Science and Engineering major.</p>
-        <p>Our team members are:</p>
+        <p>Developers:</p>
         <ul>
-          <li>Andrew Nguyen</li>
-          <li>Andrew Nguyen</li>
-          <li>Andrew Nguyen</li>
-          <li>Andrew Nguyen</li>
+          <li>Head Frontend Developer: Logan Dracos</li>
+          {/* paragraph description */}
+          <h5>
+            Currently studying at Duke University and planning to major in Computer Science with a minor in Finance. 
+            <br></br>
+            <br></br>
+            Avid Celtics fan and just love the NBA and its analytics. Originally got interested in NBA deep-dives through Ben Taylor's Thinking Basketball podcast.
+            <br></br>
+            <br></br>
+            Main coding experience using react and python for projects similar to this one (check out my github linked in contact us).
+            <br></br>
+            <br></br>
+            Favorite player: Jayson Tatum
+            <br></br>
+            <br></br>
+            Player Comp: Jayson Tatum (I swear)
+
+          </h5>
+
+          <li>Head Backend Developer: Ethan Waggoner</li>
         </ul>  
 
     </div>
--- src/components/ContactContent.js
+++ src/components/ContactContent.js
@@ -4,33 +4,22 @@ const ContactContent = () => {
   return (
     <div class="scrollable-content">
 
-      My grandma passed away yesterday and who comes running in right after but LeMickey!! He snatches the ring right off of her cold dead fingers and runs away. Too bad he doesn’t know that my grandfather proposed to her at Disney world. Just another Mickey Mouse ring for LeMickey.
+  <p>Contact Us:</p>
+        <ul>
+          <li>Logan Dracos</li>
+          {/* paragraph description */}
+          <h5>
+            Email: logandracos@icloud.com
+            <br></br>
+            <br></br>
+            <a href="https://www.linkedin.com/in/logandracos" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <br></br>
+            <br></br>
+            <a href="https://www.github.com/dracos-l" target="_blank" rel="noopener noreferrer">Github</a>
+          </h5>
 
-On the 12th day of Christmas LeMickey gave to me
-
-12 excuses given
-
-11 choked quarters
-
-10 faked finals
-
-9 tears to the refs
-
-8 missed free throws
-
-7 flops and travels
-
-6 finals losses
-
-5 turnovers
-
-4 fake rings
-
-3 superteams
-
-2 times saved
-
-And an undeserved Mickey Mouse ring
+          <li>Ethan Waggoner</li>
+        </ul>  
 
     </div>
   );
