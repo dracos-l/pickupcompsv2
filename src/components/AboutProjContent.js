1e74a261332dbee87319555a84e581324351d437
Ethan Waggoner <edubs5@Ethans-MacBook-Air.local>
2024-02-06 18:22:38 -0600

    about project

--- src/components/AboutProjContent.js
+++ src/components/AboutProjContent.js
@@ -4,13 +4,17 @@ const AboutProjContent = () => {
   return (
     <div class='about'>
         <h1>About Project Page</h1>
-        <p>Things that are gonna go on here: (btw def thinking of making this like interactive/have a lot of stuff going on, not just a page w text) (also this will probs be similar to the readme)</p>
-        <ul>
-          <li>Frontend stack/development</li>
-          <li>Backend w/ nba API and storing data</li>
-          <li>Philosophical choices in terms of what data to use</li>
-          <li>Frontend design choices, pursuing the most accurate comparison</li>
-        </ul>
+        <h2>Philosphy</h2>
+        <p>
+          From a philosphical standpoint, our goal was to take the debate you have at the courts about who you play like more serious level than anyone has before us.
+          Other website that compare you to NBA players are buzz-feed style quizzes that are fun but not very accurate. Where are you from? What's your favorite color? What's your favorite food? These are all questions that are fun to answer but don't really have anything to do with how you play basketball.
+          We wanted to take a more accurate, data-driven approach to this question. Our goal was to answer questions hoopers would actually know about there game but that were also relevant in differentiating between play styles, strengths and weaknesses.
+          We guarentee that if you are genuiely honest about your game, we can give you a more accurate list of players you play like than any other website in the world.
+        </p>
+        <h2>Front-End</h2>
+        <p>The front-end is honestly the easiest part. I just used Chat GPT to pick the prettiest colors</p>
+        <h2>Back-End</h2>
+        <p>The back-end connects to an API that scraped all of NBA.com’s stats from their website. Using this API, I compiled a file of all relevant stats for every player that qualified and converted this to percentiles on where they ranked for each stat. I then created a JS file with functions that our react website could use in order to calculate a display of the top ten players the users are similar too and specific highlights like what categories the user and top player had most and least in common. </p>
     </div>
   );
 };
--- src/components/ContactContent.js
+++ src/components/ContactContent.js
@@ -21,6 +21,19 @@ const ContactContent = () => {
           </h5>
 
           <li>Ethan Waggoner</li>
+          <h5>
+            waggonere@uchicago.edu
+            <br></br>
+            <br></br>
+            <a href="https://www.linkedin.com/in/ethan-waggoner-a6136627b/" target="_blank" rel="noopener noreferrer">
+              <img src="https://static-00.iconduck.com/assets.00/linkedin-icon-1024x1024-net2o24e.png" class="linkedIn" alt="linkedInL"/>
+            </a>
+            <br></br>
+            <br></br>
+            <a href="https://github.com/Edubs2003" target="_blank" rel="noopener noreferrer">
+              <img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Github-512.png" class="github" alt = "linkedInL"/>
+            </a>
+          </h5>
         </ul>  
 
     </div>
