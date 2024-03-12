aaccb1fb3de18a92a64136d8ecbed0987d899436
Ethan Waggoner <edubs5@Ethans-MacBook-Air.local>
2024-03-11 23:12:37 -0400

    highlights v2

--- src/calcuation/calculateSimilarity.js
+++ src/calcuation/calculateSimilarity.js
@@ -120,7 +120,6 @@ function highlights(similarityDic) {
     let dic = {};
     let similarity1 = Object.entries(similarityDic)
                             .sort((a, b) => a[1]['Similarity Score'] - b[1]['Similarity Score'])[0];
-
     // Apply the transformation to Offense_value and Defense_value with rounding
     dic['Offense_value'] = ((1 - similarity1[1]['Offense']) * 100).toFixed(2);
     dic['Defense_value'] = ((1 - similarity1[1]['Defense']) * 100).toFixed(2);
@@ -132,12 +131,10 @@ function highlights(similarityDic) {
     // Find the key of the minimum and maximum value in the similarity1[1] dictionary
     let keyOfMinValue = Object.keys(similarity1[1]).reduce((a, b) => similarity1[1][a] < similarity1[1][b] ? a : b);
     let keyOfMaxValue = Object.keys(similarity1[1]).reduce((a, b) => similarity1[1][a] > similarity1[1][b] ? a : b);
-
-    // Apply the transformation and rounding to "Most Similar Stat" and "Least Similar Stat"
     dic['Most_similar_value'] = `${keyOfMinValue}: ${((1 - similarity1[1][keyOfMinValue]) * 100).toFixed(2)}`;
     dic['Least_similar_value'] = `${keyOfMaxValue}: ${((1 - similarity1[1][keyOfMaxValue]) * 100).toFixed(2)}`;
-
     return dic;
 }
+
 
 export { calculation, results, highlights };
