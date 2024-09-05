22cf8970f13cd2429ac6c8c4479420c3997f660b
dracos-l <logandracos@icloud.com>
2024-09-05 19:04:07 +0200

    Added Jordan

--- src/components/AboutUsContent.js
+++ src/components/AboutUsContent.js
@@ -41,6 +41,24 @@ const AboutUsContent = () => {
             <br></br>
             Player Comp: Jalen Suggs 
           </h5>
+
+          <li>Graphic Designer: Jordan Wasserberger</li>
+          <h5>  
+            Currently studying at Harvard College and planning to major in Economics
+            <br></br>
+            <br></br>
+            Religious hockey fanatic but love sports analytics and statistics in general. Pretty cool to be able to know precisely how bad at basketball I am.
+            <br></br>
+            <br></br>
+            Main coding/graphic design experience is using C++, Adobe Suite, and Unreal Engine for game development and video production.
+            <br></br>
+            <br></br>
+            Favorite player: Jalen Brunson
+            <br></br>
+            <br></br>
+            Player Comp: Are there any NBA players missing a frontal lobe and/or hands? That’s probably about where I’m at. Might be a little worse. 
+          </h5>
+
         </ul>  
 
     </div>
--- src/components/ContactContent.js
+++ src/components/ContactContent.js
@@ -34,6 +34,16 @@ const ContactContent = () => {
               <img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Github-512.png" class="github" alt = "linkedInL"/>
             </a>
           </h5>
+
+          <li>Jordan Wasserberger</li>
+          <h5>
+            jwasserberger@college.harvard.edu
+            <br></br>
+            <br></br>
+            <a href="https://www.linkedin.com/in/jordan-wasserberger-b41113243?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">
+              <img src="https://static-00.iconduck.com/assets.00/linkedin-icon-1024x1024-net2o24e.png" class="linkedIn" alt="linkedInL"/>
+            </a>
+          </h5>
         </ul>  
 
     </div>
