68ad26f050d676dbe7459f6986c2acf896fe8283
Ethan Waggoner <edubs5@Ethans-MacBook-Air-2.local>
2025-12-16 17:21:43 -0500

    fix express wildcard

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use('/api/user', userRoutes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
 
-    app.get('*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
 }
